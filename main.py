160439f9edab78c26167c53eaea6cec3f0e4a7c1
Wilson McDade <wilson.mcdade@gmail.com>
2021-04-09 12:08:19 -0400

    Added Screenmanager and view switching buttons

--- main.py
+++ main.py
@@ -16,6 +16,25 @@ from kivy.app import App
 from kivy.core.window import Window
 from kivy.lang.builder import Builder
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+
+class HomeScreen(Screen):
+    pass
+
+class NewLabel(Screen):
+    pass
+
+class AutoScan(Screen):
+    pass
+
+class ItemLookup(Screen):
+    pass
+
+class MainWindow(Screen):
+    pass
+
 
 # Kivy app
 class fridgr(App):
@@ -27,10 +46,32 @@ class fridgr(App):
     '''
     def build(self):
         Window.clearcolor = (.85,.85,.85,1)
-        #Window.show_cursor = False
+        # Window.show_cursor = False
 
-        root= Builder.load_file('fridgr.kv')
+        root = BoxLayout()
+        
+        root.add_widget(Builder.load_file('left.kv'))
+
+        smwindow = Builder.load_file('fridgr.kv')
+
+        self.sm = ScreenManager(transition=NoTransition())
+        self.sm.add_widget(HomeScreen())
+        self.sm.add_widget(NewLabel())
+        self.sm.add_widget(AutoScan())
+        self.sm.add_widget(ItemLookup())
+        root.add_widget(self.sm)
+
+        self.sm.current = 'HomeScreen'
+
+        root.add_widget(Builder.load_file('right.kv'))
+
         return root
+
+    '''
+    btnprs interprets a buttonpress
+    '''
+    def btnprs(self, button):
+       pass 
 
 if __name__ == '__main__':
     Config.set('graphics','fullscreen','false')
